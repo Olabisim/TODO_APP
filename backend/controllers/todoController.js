b7a9bed0c4c2156a6e162f50083202d9588f188d
holabisii <ajoseholabisi@gmail.com>
2023-03-19 18:55:57 -0400

    update and delete todo api

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -30,4 +30,37 @@ async function retrieveTodo(_, res) {
 }
 
 
-module.exports = {createTodo, retrieveTodo}
+async function updateTodo(req, res) {
+        try {
+                const {id, title, completed} = req.body;
+
+                const todo = Todo.findOne({while: {id}})
+
+                todo.set({title, completed})
+
+                res.status(200).json({status: true, message: 'todo updated successfully'})
+
+        }
+        catch(err) {
+                res.status(500).json({status: false, message: "todo could not be updated", data: {err}})
+        }
+}
+
+async function deleteTodo(req, res) {
+        try {
+                const {id} = req.params;
+                console.log(id)
+
+                const todo = Todo.Destroy({where: {id}})
+
+                res.status(200).json({status: true, message: 'todo deleted successfully', data: {todo}})
+
+        }
+        catch(err) {
+                res.status(500).json({status: false, message: "todo could not be delete", data: {err}})
+        }
+
+}
+
+
+module.exports = {createTodo, retrieveTodo, updateTodo, deleteTodo}
--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -6,6 +6,8 @@ const router = express.Router()
 
 router.post('/todos', todoController.createTodo)
 router.get('/todos', todoController.retrieveTodo)
+router.post('/todos/update', todoController.updateTodo)
+router.delete('/todos/delete/:id', todoController.deleteTodo)
 
 
 module.exports = router
