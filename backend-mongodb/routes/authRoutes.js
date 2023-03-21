e0ef1ee11b406415bd259e3a0d2867040f9ce9ae
holabisii <ajoseholabisi@gmail.com>
2023-03-21 15:01:30 -0400

    added login endpoint

--- backend-mongodb/routes/authRoutes.js
+++ backend-mongodb/routes/authRoutes.js
@@ -4,5 +4,6 @@ import { signup } from '../controllers/authController.js';
 const router = express.Router();
 
 router.post('/signup', signup);
+router.post('/login', login);
 
 export default router;
